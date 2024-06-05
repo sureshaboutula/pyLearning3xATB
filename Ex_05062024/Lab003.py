22d20452dc4c73f3f0f4380473f48f7de4458588
padal <aboutula.suresh@gmail.com>
2024-06-05 19:58:29 -0400

    Update Lab003

--- Ex_05062024/Lab003.py
+++ Ex_05062024/Lab003.py
@@ -1,10 +1,14 @@
-# print()
+# print(self, *args, sep=' ', end='\n', file=None)
 
-# self,
-# *args,
-# sep=' ',
-# end='\n',
-# file=None - File IO
+# self - Concept in OOPS
+# *args - Unlimited number of  arguments - Integer, Float, String, Boolean....
+# sep=' ' - How you want to separate the arguments
+# end='\n' - in the end what you want
+# file=None - File IO concept
 
-print("I am a good person", end="_")
-print("I am a bad person")
+print("Hello", "World", 123, True, 3.14) # Default Separator is space -- same as next line
+print("Hello", "World", 123, True, 3.14, sep=" ")
+print("Hello", "World", 123, True, 3.14, sep="_")
+print("Hi, My name is Suresh", "Aboutula")
+print("Hi, My name is Suresh", "Aboutula", sep=" - ")
+
