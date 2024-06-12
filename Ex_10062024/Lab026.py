f0a23668839e22ce1a0522acae5eb42432f7b93d
padal <aboutula.suresh@gmail.com>
2024-06-11 21:18:14 -0400

    June 10 - Literals and Operators

---
+++ Ex_10062024/Lab026.py
@@ -0,0 +1,8 @@
+# Set
+my_set = {1, 2, 3, 4, 5, 5} # Collection of unique items
+print(my_set) # {1, 2, 3, 4, 5}
+
+newline_char = '\n'
+newtab_char = '\t'
+newbackspace_char = '\b'
+
---
+++ Ex_10062024/Lab027.py
@@ -0,0 +1,31 @@
+# Bool literals
+is_married = True
+is_suresh_married = False
+
+# Decimal literal
+age = 35
+
+# Binary literal
+number_ten = 0b1010 # 10
+print(number_ten)
+
+#Octal literal
+c = 0o130 # 88
+print(c)
+
+#Hexadecimal literal
+d = 0x12c # 300
+print(d)
+
+# String literals
+
+# single quote:
+name = 'Suresh'
+
+# double quotes:
+name2 = "Suresh"
+
+# triple quotes for multi line string
+mul_line_str = """This is
+                    multiple lines
+                    string"""
